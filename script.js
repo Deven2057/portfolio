5666ac97ab78c9572871f101a1548b1c50bba5c1
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2024-01-25 19:17:11 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+function downloadResume() {
+  // Replace 'path/to/your/resume.pdf' with the actual path to your resume file
+  var resumeUrl ='devenCV.docx.pdf';
+
+  var link = document.createElement('a');
+  link.href = resumeUrl;
+  link.download = 'deven-resume.pdf'; // You can set the desired file name
+  document.body.appendChild(link);
